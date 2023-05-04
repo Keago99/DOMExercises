2d78faef61175820d76c2776089a0409b2895f3b
Keago99 <keatabde@gmail.com>
2023-05-04 15:48:32 +0200

    added blue h3 heading through scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,22 @@
 
-const firstDiv = document.createElement('div');
-const para = document.createElement('p');
+
 const body = document.querySelector('body');
 
+const firstDiv = document.createElement('div');
+const para = document.createElement('p');
+const blueHeading = document.createElement('h3');
+
 firstDiv.classList.add("firstDiv");
+
 para.innerHTML = "Hey I'm red!";
 para.style.cssText = 'color: red';
 
+blueHeading.classList.add("blueHead");
+blueHeading.style.cssText = 'color: blue';
+blueHeading.innerHTML = "I'm a blue H3";
+
+
 body.appendChild(firstDiv);
 firstDiv.appendChild(para);
+firstDiv.appendChild(blueHeading);
 
