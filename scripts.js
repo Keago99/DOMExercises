fe143eaaf5bc70e55bb7d1de2207dcb5880d6c59
Keago99 <keatabde@gmail.com>
2023-05-04 16:39:52 +0200

    add second Div with CSS text. modified scripts.js

--- scripts.js
+++ scripts.js
@@ -5,8 +5,10 @@ const body = document.querySelector('body');
 const firstDiv = document.createElement('div');
 const para = document.createElement('p');
 const blueHeading = document.createElement('h3');
+const secondDiv = document.createElement('div')
 
 firstDiv.classList.add("firstDiv");
+
 
 para.innerHTML = "Hey I'm red!";
 para.style.cssText = 'color: red';
@@ -15,8 +17,11 @@ blueHeading.classList.add("blueHead");
 blueHeading.style.cssText = 'color: blue';
 blueHeading.innerHTML = "I'm a blue H3";
 
+secondDiv.style.cssText = "background-color: pink; border-color: black;";
+secondDiv.innerHTML = "testo besto";
 
 body.appendChild(firstDiv);
 firstDiv.appendChild(para);
 firstDiv.appendChild(blueHeading);
+body.appendChild(secondDiv);
 
