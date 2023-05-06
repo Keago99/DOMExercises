a59306c67b50ccc8b2d3c449873319b2c0f4433a
Keago99 <keatabde@gmail.com>
2023-05-06 20:18:14 +0200

    final commit

--- scripts.js
+++ scripts.js
@@ -6,10 +6,11 @@ const firstDiv = document.createElement('div');
 const para = document.createElement('p');
 const blueHeading = document.createElement('h3');
 const secondDiv = document.createElement('div')
+const secondPara = document.createElement('p');
 
 firstDiv.classList.add("firstDiv");
 
-
+para.classList.add("firstPara")
 para.innerHTML = "Hey I'm red!";
 para.style.cssText = 'color: red';
 
@@ -20,8 +21,11 @@ blueHeading.innerHTML = "I'm a blue H3";
 secondDiv.style.cssText = "background-color: pink; border-color: black;";
 secondDiv.innerHTML = "testo besto";
 
+secondPara.innerHTML = "ME TOO!";
+
 body.appendChild(firstDiv);
 firstDiv.appendChild(para);
 firstDiv.appendChild(blueHeading);
 body.appendChild(secondDiv);
+secondDiv.appendChild(secondPara);
 
